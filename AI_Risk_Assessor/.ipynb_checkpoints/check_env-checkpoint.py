e1cd1b48ebbe4f5ab642cf388a77b7f36ccba09a
zahirkhanBattani <zahirkhanbettaniuk@gmail.com>
2025-10-27 16:18:59 +0000

    initial commit - AI Risk Assessor Project

---
+++ AI_Risk_Assessor/.ipynb_checkpoints/check_env-checkpoint.py
@@ -0,0 +1,10 @@
+import sys
+import numpy as np
+import pandas as pd
+
+# Check Python executable
+print("Python executable:", sys.executable)
+
+# Check versions of key packages
+print("NumPy version:", np.__version__)
+print("Pandas version:", pd.__version__)
